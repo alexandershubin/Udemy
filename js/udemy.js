69597f8dc11e561ef6eef3bcbf9ac6916a1ef6f8
Alexandr <exemple.@gmail.com>
2019-10-07 07:13:06 +0300

    add second task

---
+++ js/udemy.js
@@ -0,0 +1,50 @@
+// 'use strict';
+
+// (function(){
+
+// 	if(num < 49) {
+// 		console.log("Не верно!");
+// 	} else if(num > 100) {
+// 		console.log("много");
+// 	} else {
+// 		console.log("Верно!");
+// 	}
+
+// 	(num = 50) ? console.log("Верно!") : console.log("Верно!");
+
+// 	switch (num) {
+// 		case num < 49:
+// 			console.log("Не верно!");
+// 			break;
+// 		case num > 100:
+// 			console.log("много");
+// 			break;
+// 		case num > 80:
+// 			console.log("все еще много");
+// 			break;
+// 		case 50:
+// 			console.log("Верно!");
+// 			break;
+// 		default: console.log("что то пошло не так");
+// 		break;
+// 	}
+
+// 	let num = 50;
+// 	// while (num < 55) {
+// 	// 	console.log(num);
+// 	// 	num++;
+// 	// }
+
+// 	do {
+// 		console.log(num);
+// 		num++;
+// 	}
+// 	while (num < 55);
+
+// 	for(let i = 0; i < 8; i++) {
+// 		if(i == 6) {
+// 			continue;
+// 		}
+// 		console.log(i);
+// 	}
+// })();
