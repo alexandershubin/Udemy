053bd6f31d9e16710138bdd1cd5441af91db2db4
Alexandr <exemple.@gmail.com>
2019-10-18 08:01:22 +0300

    add task 5

--- js/udemy.js
+++ js/udemy.js
@@ -108,10 +108,48 @@
 //     i = arr.join(', ');
 //     console.log(i);
 
-let arr = [1, 15, 4],
-     i = arr.sort(compareNem);
-     function compareNem(a,b) {
-      return a-b;
-     }
-     console.log(arr);
+// let arr = [1, 15, 4],
+//      i = arr.sort(compareNem);
+//      function compareNem(a,b) {
+//       return a-b;
+//      }
+//      console.log(arr);
+
+// console.log(0 || "" || 2 || undefined || true || falsе);
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'),
+    circle = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper');
+
+    box.style.backgroundColor = 'blue';
+    btn[1].style.borderRadius = '100%';
+    circle[0].style.backgroundColor = 'red';
+    circle[1].style.backgroundColor = 'yellow';
+    circle[2].style.backgroundColor = 'green';
+
+    // for(let i = 0; i < heart.length; i++) {
+    //   heart[i].style.backgroundColor = 'blue';
+    // }
+    // heart.forEach(function(item, i, hearts) {
+    //   item.style.backgroundColor = 'blue';
+    // });
+
+    let div = document.createElement('div'),
+        text = document.createTextNode('я учу JS');
+
+        div.classList.add('black');
+        // document.body.appendChild(div);
+        // wrapper.appendChild(div);
+
+      // div.innerHTML = '<h1>я очень хочу выучить js</h1>';
+      div.textContent = 'я очень хочу выучить js';
+
+        document.body.insertBefore(div, circle[0]);
+        document.body.removeChild(circle[1]);
+        wrapper.removeChild(heart[1]);
+
+        document.body.replaceChild(btn[1], circle[1]);
+    console.log(div);
 })();
